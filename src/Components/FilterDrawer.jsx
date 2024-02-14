6a41bc7ccb31bfdec1e01a6d1e5dbbc0b468e30f
Michele Granatiero <mikigranatiero@hotmail.com>
2024-02-14 11:09:47 +0100

    update filters

--- src/Components/FilterDrawer.jsx
+++ src/Components/FilterDrawer.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Drawer,
   DrawerClose,
@@ -8,29 +8,63 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
-
+} from "@/components/ui/drawer";
+import { ToggleGroupItem } from "@/components/ui/toggle-group";
+import { LuSettings2 } from "react-icons/lu";
 
 function FilterDrawer() {
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <Button variant="outline">Filters</Button>
+        <Button variant="outline" className="gap-2 text-zinc-500 text-md">
+          <div> Filters </div>
+          <LuSettings2 size={20}/>
+        </Button>
       </DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Filters</DrawerTitle>
-          <DrawerDescription>Choose your filters.</DrawerDescription>
-        </DrawerHeader>
-        <DrawerFooter>
-          <Button>Submit</Button>
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
+        <div className="mx-auto flex flex-col w-full max-w-sm p-4 mb-4 whitespace-nowrap ">
+          <DrawerHeader className="sm:text-center">
+            <DrawerTitle>Filters</DrawerTitle>
+          </DrawerHeader>
+          <div className="flex flex-col items-stretch gap-1 p-4 text-zinc-500">
+            <h4 className="mb-2 text-sm font-medium leading-none ">State</h4>
+            <ToggleGroupItem value="completed" aria-label="Toggle completed">
+              <span>completed</span>
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="in progress"
+              aria-label="Toggle in progress">
+              <span>in progress</span>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="pending" aria-label="Toggle pending">
+              <span>pending</span>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="canceled" aria-label="Toggle canceled">
+              <span>canceled</span>
+            </ToggleGroupItem>
+          </div>
+          <div className="flex flex-col items-stretch gap-1 p-4 text-zinc-500">
+            <h4 className="mb-2 text-sm font-medium leading-none">My Role</h4>
+            <ToggleGroupItem value="issuer" aria-label="Toggle issuer">
+              <span>issuer</span>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="funder" aria-label="Toggle funder">
+              <span>funder</span>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="worker" aria-label="Toggle worker">
+              <span>worker</span>
+            </ToggleGroupItem>
+          </div>
+          <DrawerFooter>
+            {/* <Button>Submit</Button> */}
+            <DrawerClose asChild>
+              <Button variant="outline">Close</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
       </DrawerContent>
     </Drawer>
-  )
+  );
 }
 
-export default FilterDrawer
+export default FilterDrawer;
--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,60 +1,56 @@
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-import { useEffect, useState } from "react"
-import FilterDrawer from "./FilterDrawer"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { useEffect, useState } from "react";
+import FilterDrawer from "./FilterDrawer";
 
-function Filters({className = ""}) {
-
+function Filters({ className = "" }) {
   const [filter, setFilter] = useState(["in progress", "pending"]);
 
   useEffect(() => {
-    console.log(filter)
+    console.log(filter);
   }, [filter]);
 
   return (
-    <ToggleGroup type="multiple" variant="outline" defaultValue={filter} 
+    <ToggleGroup
+      type="multiple" variant="outline" defaultValue={filter}
       onValueChange={(value) => setFilter(value)}
-      className={`flex-col self-stretch justify-start items-start ${className}`}>
+      className={`flex-col items-start justify-start self-stretch ${className}`}>
       <div className="px-4 md:hidden">
         <FilterDrawer />
       </div>
-      <aside className="p-4 text-zinc-600  hidden md:flex flex-col gap-y-4 whitespace-nowrap sticky top-14 ">
-          <h3 className="mt-4 pb-2 text-lg font-medium leading-none border-b-2">
-            Filters
-          </h3>
-          <div
-            className="flex flex-col items-stretch gap-1">
-            <h4 className="mb-2 text-sm font-medium leading-none">State</h4>
-            <ToggleGroupItem value="completed" aria-label="Toggle completed">
-              <span>completed</span>
-            </ToggleGroupItem>
-            <ToggleGroupItem value="in progress" aria-label="Toggle in progress">
+      <aside className="sticky top-14 hidden flex-col gap-y-4 whitespace-nowrap p-4 text-zinc-500 md:flex ">
+        <h3 className="mt-4 border-b-2 pb-2 text-lg font-medium leading-none">
+          Filters
+        </h3>
+        <div className="flex flex-col items-stretch gap-1">
+          <h4 className="mb-2 text-sm font-medium leading-none">State</h4>
+          <ToggleGroupItem value="completed" aria-label="Toggle completed">
+            <span>completed</span>
+          </ToggleGroupItem>
+          <ToggleGroupItem value="in progress" aria-label="Toggle in progress">
             <span>in progress</span>
-            </ToggleGroupItem>
-            <ToggleGroupItem value="pending" aria-label="Toggle pending">
+          </ToggleGroupItem>
+          <ToggleGroupItem value="pending" aria-label="Toggle pending">
             <span>pending</span>
-            </ToggleGroupItem>
-            <ToggleGroupItem value="canceled" aria-label="Toggle canceled">
+          </ToggleGroupItem>
+          <ToggleGroupItem value="canceled" aria-label="Toggle canceled">
             <span>canceled</span>
-            </ToggleGroupItem>
-          </div>
-          <div
-            className="flex flex-col items-stretch gap-1">
-            <h4 className="mb-2 text-sm font-medium leading-none">My Role</h4>
-            <ToggleGroupItem value="issuer" aria-label="Toggle issuer">
-              <span>issuer</span>
-            </ToggleGroupItem>
-            <ToggleGroupItem value="funder" aria-label="Toggle funder">
+          </ToggleGroupItem>
+        </div>
+        <div className="flex flex-col items-stretch gap-1">
+          <h4 className="mb-2 text-sm font-medium leading-none">My Role</h4>
+          <ToggleGroupItem value="issuer" aria-label="Toggle issuer">
+            <span>issuer</span>
+          </ToggleGroupItem>
+          <ToggleGroupItem value="funder" aria-label="Toggle funder">
             <span>funder</span>
-            </ToggleGroupItem>
-            <ToggleGroupItem value="worker" aria-label="Toggle worker">
+          </ToggleGroupItem>
+          <ToggleGroupItem value="worker" aria-label="Toggle worker">
             <span>worker</span>
-            </ToggleGroupItem>
-          </div>
-        
+          </ToggleGroupItem>
+        </div>
       </aside>
-      
     </ToggleGroup>
-  )
+  );
 }
 
-export default Filters
+export default Filters;
