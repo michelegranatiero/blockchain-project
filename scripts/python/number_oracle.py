514d9c60577b31aea263e85d1eeebb53809a9ecc
Michele Granatiero <mikigranatiero@hotmail.com>
2024-02-28 13:26:59 +0100

    comments

--- scripts/python/number_oracle.py
+++ scripts/python/number_oracle.py
@@ -12,6 +12,8 @@ f.close()
 contract_address = Web3.to_checksum_address('0x5fbdb2315678afecb367f032d93f642f64180aa3')
 contract = w3.eth.contract(address=contract_address, abi=abi)
 
+# Master contract should emit event and specify the address of the task contract
+
 event_filter = contract.events['NeedRandomness'].create_filter(fromBlock='latest')
 
 accounts = w3.eth.accounts
@@ -21,6 +23,7 @@ print('Oracle is ready to serve...\n')
 try:
     while True:
         for event in event_filter.get_new_entries():
+            # retrive specific task address from event (in smart contract: put address inside event)
             rnd = [random.randint(0,event.args['upperBound'] - 1) for _ in range(event.args['numberOfSeeds'])]
             print(f"Sending randomness to Smart Contract at address: {contract_address}...")
             print(f"\nSelected randomness is: {rnd}")
